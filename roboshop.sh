af15bf658acd45fadc12640e04d25b34e6f1715e
Ram Kumar <rmkm2034@gmail.com>
2026-01-06 01:18:54 +0530

    hello-world started

--- roboshop.sh
+++ roboshop.sh
@@ -10,7 +10,7 @@ DOMAIN_NAME="ramdevops.site"
 for instance in ${INSTANCES[@]}
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids  sg-0f9e10b0b5e242043 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-    if [ instance != "fronend" ]
+    if [ $instance != "fronend" ]
     then
        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else

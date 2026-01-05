8cf705e64b5ee5b3e2ee8a72320f7cd12215d2e0
Ram Kumar <rmkm2034@gmail.com>
2026-01-06 00:53:54 +0530

    hello-world started

--- roboshop.sh
+++ roboshop.sh
@@ -9,12 +9,12 @@ DOMAIN_NAME="ramdevops.site"
 
 for instance in ${instance[@]}
 do
-    aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$INSTANCE}]" --query "Instances[0].PrivateIpAddress" --output text
+    INSTANCE_ID=aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-01bc7ebe005fb1cb2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$INSTANCE}]" --query "Instances[0].PrivateIpAddress" --output text
     if[ instance != "fronend" ]
     then
-       IP=$(aws ec2 describe-instances --instance-ids i-0386c51dfee0f7031 --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
-       IP=$(aws ec2 describe-instances --instance-ids i-0386c51dfee0f7031 --query "Reservations[0].Instances[0].PublicIpAddress" --output text)   
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)   
     fi
     echo "$INSTANCE IP Address : $IP"    
 done

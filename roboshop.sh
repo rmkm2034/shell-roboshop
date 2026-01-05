8f5ffcae65eda680230a02b4d41a13ad07af6c86
Ram Kumar <rmkm2034@gmail.com>
2026-01-06 01:07:31 +0530

    hello-world started

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ ZONE_ID="Z1011650191JZ4JYJQ4FE"
 DOMAIN_NAME="ramdevops.site"
 
 
-for instance in ${instance[@]}
+for instance in ${INSTANCES[@]}
 do
     INSTANCE_ID=aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids  sg-0f9e10b0b5e242043 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].PrivateIpAddress" --output text
     if [ instance != "fronend" ]

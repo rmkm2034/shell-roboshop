09c25b0f04a0f668ac8293fa9d07651e61164fe8
Ram Kumar <rmkm2034@gmail.com>
2026-01-08 00:17:32 +0530

    hello-world started

--- roboshop.sh
+++ roboshop.sh
@@ -17,4 +17,22 @@ do
        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)   
     fi
     echo "$INSTANCE IP Address : $IP"    
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "creating or updating a record set"
+    ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'" $instance "'.'$DOMAIN_NAME'"
+        ,"Type"             : "CNAME"
+        ,"TTL"              : 120
+        ,"ResourceRecords"  : [{
+            "Value"         : "'" $IP "'"
+        }]
+      }
+    }]
+  }
 done
